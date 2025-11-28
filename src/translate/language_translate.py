be41aa0b215d1f771095b3d1e3dbe1f01a894b3c
Saravanakumar Ramasamy <rsaravanakumar@athenahealth.com>
2025-11-28 10:56:30 +0530

    tmail to english translation

--- src/translate/language_translate.py
+++ src/translate/language_translate.py
@@ -25,8 +25,7 @@ if DEVICE == "mps":
 
 HF_TOKEN = os.getenv(
     "HUGGING_FACE_HUB_TOKEN",
-    ""
-    #USE YOUR OWN HUGGING_FACE_HUB_TOKEN
+    "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"
 )
 
 src_lang, tgt_lang = "tam_Taml", "eng_Latn"
